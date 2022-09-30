af98cbdb9a469f32cbb556a83901d4acbdb8caef
MrGhastien <bastounm@free.fr>
2022-10-01 00:51:01 +0200

    Changed several colors, added support for LaTeX syntax coloring

--- .config/emacs/Test-theme.el
+++ .config/emacs/Test-theme.el
@@ -9,21 +9,21 @@
  'Test
  '(cursor ((t (:background "green"))))
  '(error ((t (:underline (:color "red" :style wave)))))
- '(default ((t (:background "#1b1918":foreground "gray70" :weight semi-bold :height 125 :family "Source Code Pro"))))
-
- ;; Mode line
  '(mode-line ((t (:box (:line-width (-1 . 8) :color "#2c2726") :foreground "white smoke" :background "#2c2726"))))
  '(mode-line-inactive ((t (:box (:line-width (-1 . 8) :color "#0e0b0b") :foreground "gray30" :background "#0e0b0b"))))
-
- ;; Font lock faces
- '(font-lock-builtin-face ((t (:foreground "LightSteelBlue"))))
- '(font-lock-function-name-face ((t (:foreground "tomato"))))
- '(font-lock-keyword-face ((t (:foreground "tan2"))))
- '(font-lock-type-face ((t (:foreground "medium slate blue"))))
- '(font-lock-variable-name-face ((((class grayscale) (background light)) (:slant italic :weight bold :foreground "Gray90")) (((class grayscale) (background dark)) (:slant italic :weight bold :foreground "DimGray")) (((class color) (min-colors 88) (background light)) (:foreground "sienna")) (((class color) (min-colors 88) (background dark)) (:foreground "LightGoldenrod")) (((class color) (min-colors 16) (background light)) (:foreground "DarkGoldenrod")) (((class color) (min-colors 16) (background dark)) (:foreground "LightGoldenrod")) (((class color) (min-colors 8)) (:weight light :foreground "yellow")) (t (:slant italic :weight bold))))
- '(font-lock-string-face ((t (:foreground "dark salmon"))))
- '(font-lock-constant-face ((t (:weight bold :inherit font-lock-variable-name-face))))
- '(font-lock-comment-face ((t (:foreground "gray50"))))
-)
+ '(font-lock-builtin-face ((t (:foreground "turquoise"))))
+ '(font-lock-function-name-face ((t (:foreground "#ff5d4a"))))
+ '(font-lock-keyword-face ((t (:foreground "#ff7800"))))
+ '(font-lock-type-face ((t (:foreground "#ee9f53"))))
+ '(font-lock-variable-name-face ((t (:foreground "bisque"))))
+ '(font-lock-string-face ((t (:foreground "PaleGreen1"))))
+ '(font-lock-constant-face ((t (:inherit font-lock-variable-name-face :weight bold))))
+ '(font-lock-comment-face ((t (:foreground "gray50" :slant italic))))
+ '(font-latex-math-face ((t (:inherit font-lock-string-face :weight normal))))
+ '(font-latex-italic-face ((t (:inherit italic :foreground "misty rose"))))
+ '(font-latex-script-char-face ((t (:foreground "cyan"))))
+ '(italic ((t (:slant italic))))
+ '(font-latex-sectioning-5-face ((t (:weight extra-bold :underline t :inherit font-lock-function-name-face))))
+ '(default ((t (:background "#1b1918" :foreground "gray70" :weight semi-bold :height 130 :family "Source Code Pro")))))
 
 (provide-theme 'Test)
