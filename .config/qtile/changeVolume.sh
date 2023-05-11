db4b29eccd8d8b52470c0f570d426a61d720b281
MrGhastien <bastounm@free.fr>
2023-05-11 15:00:39 +0200

    Change volume changing script to use wireplumber instead of amixer

--- .config/qtile/changeVolume.sh
+++ .config/qtile/changeVolume.sh
@@ -2,7 +2,11 @@
 
 msgTag="volume-indic"
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ "$@"
+if [[ "$1" -eq "mute" ]]; then
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+else
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ "$@"
+fi
 
 mixerOutput="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | tail -1)"
 volume="$(echo ${mixerOutput} | awk '{print $2}')"
