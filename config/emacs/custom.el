37b258497da494bab13c1669af79d0efef371771
Bastien Morino <bastien.morino@free.fr>
2026-03-04 19:42:23 +0100

    Update org & evil config (emacs), make small QoL tweaks to hyprland and kitty

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -5,18 +5,22 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
-     '("3910f8354007f61530f3a0af30c6d3d0d62b004876a7391ab60379d0aebcb494"
+   '("a463f6d8bdd694c7a76e1d65000bdbf61af36e65c8a2bd9f8418e7599a368eaa"
+     "6a1d4062edcdfd31f498112a8a2fd12bb9ae188cc1f346bf7bfcc378f195d7f7"
+     "3910f8354007f61530f3a0af30c6d3d0d62b004876a7391ab60379d0aebcb494"
      default))
  '(package-selected-packages
    '(ada-mode ada-ts-mode auctex bui company-box corfu counsel dashboard
-              dockerfile-mode dts-mode emmet-mode evil f general
-              indent-bars ivy-rich js2-mode k8s-mode kubed kubel
-              kubernetes ligature lua-mode magit markdown-mode merlin
-              org-bullets org-modern org-super-agenda pdf-tools
-              qml-mode rust-mode spinner svg-lib
-              treemacs-all-the-icons tuareg typescript-mode
-              typst-ts-mode visual-fill-column web-mode websocket
-              which-key yaml-pro yasnippet)))
+              dockerfile-mode dts-mode emmet-mode evil evil-collection
+              f general indent-bars ivy-rich js2-mode k8s-mode kubed
+              kubel kubernetes ligature lua-mode magit markdown-mode
+              merlin org-bullets org-capture org-gcal org-modern
+              org-novelist org-super-agenda pdf-tools qml-mode
+              rust-mode spinner svg-lib treemacs-all-the-icons tuareg
+              typescript-mode typst-ts-mode visual-fill-column
+              web-mode websocket which-key yaml-pro yasnippet))
+ '(package-vc-selected-packages
+   '((org-novelist :url "https://github.com/sympodius/org-novelist.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- utilities/changeVolume.sh
+++ utilities/changeVolume.sh
@@ -14,10 +14,10 @@ volume="$(echo "${volume}*100" | bc)"
 volume="${volume%.*}"
 status="$(echo ${mixerOutput} | awk '{print $3}')"
  
-if [[ -z "$status" ]]; then 
-    dunstify -a "changeVolume" -u low -t 1500 -h string:x-dunst-stack-tag:$msgTag \
-             -h int:value:"$volume" "Volume : ${volume}%"
-else
-    dunstify -a "changeVolume" -u low -t 1500 -h string:x-dunst-stack-tag:$msgTag \
-             -h int:value:"$volume" "Volume : ${volume}% (Muted)"
-fi
+# if [[ -z "$status" ]]; then
+#     dunstify -a "changeVolume" -u low -t 1500 -h string:x-dunst-stack-tag:$msgTag \
+#              -h int:value:"$volume" "Volume : ${volume}%"
+# else
+#     dunstify -a "changeVolume" -u low -t 1500 -h string:x-dunst-stack-tag:$msgTag \
+#              -h int:value:"$volume" "Volume : ${volume}% (Muted)"
+# fi
